6cd20ab6a1b41dd52e8663665464ea8d9d241c5c
HimRahim <6210110690@psu.ac.th>
2022-03-14 21:16:50 +0700

    logout

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-
 function NavBar() {
   axios.interceptors.request.use(
     function (config) {
@@ -29,6 +28,11 @@ function NavBar() {
     };
     fecthData();
   }, []);
+
+  const handleSignOut = () => {
+    localStorage.clear();
+  }
+
   return (
     <div className="shadow-md">
       <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
@@ -95,6 +99,7 @@ function NavBar() {
                   <a
                     href="/"
                     class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    onClick={handleSignOut}
                   >
                     Sign out
                   </a>
@@ -143,7 +148,7 @@ function NavBar() {
               <li>
                 <a
                   href="/"
-                  class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   aria-current="page"
                 >
                   Home
