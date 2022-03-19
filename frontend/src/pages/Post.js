1ff1ca467f51eccd382c423927be3d40e74ab42d
HimRahim <6210110690@psu.ac.th>
2022-03-19 23:51:20 +0700

    update post

--- frontend/src/pages/Post.js
+++ frontend/src/pages/Post.js
@@ -4,7 +4,10 @@ import NavBar from '../components/NavBar'
 function Post() {
   return (
     <div>
-      <NavBar/>
+      <NavBar />
+      <div className='flex justify-center'>
+      <textarea name="" id="" cols="30" rows="10"></textarea>
+      </div>
     </div>
   )
 }
