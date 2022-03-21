a9cdb101a2d3275ec8baea23143849894dc048a8
HimRahim <6210110690@psu.ac.th>
2022-03-21 23:16:40 +0700

    add config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 const isProd = !(!process.env.NODE_ENV || process.env.NODE_ENV === 'development');
 
 const config = {
-  isPord,
+  isProd,
   apiUrlPrefix: isProd ? 'api' : 'http://localhost:8000/api'
 }
 
