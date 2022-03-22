46e974915a300f1886b8b6bd9ebd937a1e68efa9
HimRahim <6210110690@psu.ac.th>
2022-03-23 01:26:20 +0700

    update upload image

--- frontend/src/components/TextEditor.js
+++ frontend/src/components/TextEditor.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
@@ -10,6 +10,19 @@ function TextEditor() {
     console.log(editorState);
   };
 
+  const uploadImageCallBack = (file) => {
+    const imageObject = {
+      file: file,
+      localSrc: URL.createObjectURL(file),
+    }
+    console.log(imageObject)
+    return new Promise(
+      (resolve, reject) => {
+        resolve({ data: { link: imageObject.localSrc } });
+      }
+    );
+  };
+
   return (
     <div className="w-9/12 mx-auto shadow-md p-5">
       <div>
@@ -19,12 +32,23 @@ function TextEditor() {
           wrapperClassName="wrapperClassName"
           editorClassName="editorClassName"
           onEditorStateChange={onEditorStateChange}
+          toolbar={{
+            inline: { inDropdown: true },
+            list: { inDropdown: true },
+            textAlign: { inDropdown: true },
+            link: { inDropdown: true },
+            history: { inDropdown: true },
+            image: {
+              uploadCallback: uploadImageCallBack,
+              alt: { present: false, mandatory: false },
+            },
+          }}
         />
       </div>
-      <div className='flex justify-end'>
-      <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-        Post
-      </button>
+      <div className="flex justify-end">
+        <button className="bg-gray-200 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+          Post
+        </button>
       </div>
     </div>
   );
